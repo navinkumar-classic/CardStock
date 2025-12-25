e3849ec0e7e0702b831b0de822e78cef58382d17
Vishal <santaclawsers@gmail.com>
2025-12-25 14:44:03 +0530

    add turnmanager entity

---
+++ include/TurnManager.h
@@ -0,0 +1,53 @@
+/**
+* @file TurnManager.h
+ * @brief represents the entity that manages turns in the game
+ *
+ * Header file containing the entity that manages each turn.
+ *
+ * @author Vishal R<echelora.@gmail.com>
+ * @date 2025-12-25
+ * @version 0.1.0
+ * @copyright
+ *
+ */
+
+#pragma once
+
+#include <vector>
+#include <cstddef>
+
+// A turn consists of three phases:
+//   Start: Prepare the game state for the player to take actions,
+//          trigger start of turn effects.
+//   Main:  Game is played, logic is processed, engine validates actions,
+//   End:   Clean up phase, trigger end of turn effects,
+//          next turn is started.
+enum class TurnPhase {
+    Start,
+    Main,
+    End
+};
+
+class TurnManager {
+public:
+    TurnManager();
+
+    void init(size_t playerCount);
+
+    size_t getCurrentPlayerIndex() const;
+    size_t getTurnNumber() const;
+    TurnPhase getPhase() const;
+
+    bool isPlayersTurn(size_t playerIndex) const;
+
+    void nextPhase();
+    void endTurn();
+
+private:
+    size_t currentPlayerIndex_;
+    size_t turnNumber_;
+    TurnPhase currentPhase_;
+    size_t playerCount_;
+
+    void advanceToNextPlayer();
+};
